0cafabae4803239abe5451d1bea119058df910b9
mohid <mohid10587@gmail.com>
2026-01-06 17:12:45 +0600

    Stop tracking node_modules

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 //plugins/blog/index.ts
 const plugin = {
+  _id: "695cc2c0f2a6d1f729e30c87",
   name: "Blog",
-  slug: "blog",
+  slug: "blog-1",
   version: "1.0.0",
   author: "Blog Author",
   description: "Blog plugin",
-  type: "install/uninstall/skip",
-  githubUrl: "url",
+  type: "install",
+  githubUrl: "https://github.com/Mohidul10587/blog.git",
   isEnabled: true,
 };
 
