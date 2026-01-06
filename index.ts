875bb703f1e20d8543e1046803e65da80631033e
mohid <mohid10587@gmail.com>
2026-01-06 13:59:15 +0600

    Stop tracking node_modules

---
+++ index.ts
@@ -0,0 +1,13 @@
+//plugins/blog/index.ts
+const plugin = {
+  name: "Blog",
+  slug: "blog",
+  version: "1.0.0",
+  author: "Blog Author",
+  description: "Blog plugin",
+  type: "install/uninstall/skip",
+  githubUrl: "url",
+  isEnabled: true,
+};
+
+export default plugin;
